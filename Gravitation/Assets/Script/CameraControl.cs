2e444615960bf63e5b702449e83672e202860522
konata016 <k>
2019-05-10 16:51:47 +0900

    カメラ制御更新

--- Gravitation/Assets/Script/CameraControl.cs
+++ Gravitation/Assets/Script/CameraControl.cs
@@ -7,8 +7,8 @@ public class CameraControl : MonoBehaviour
     public GameObject Target;
     public Vector3 V3;
 
-    //public float Speed;
-    public float SmoothTime = 0.1f;
+    public float Speed = 20f;       //カメラが離れたとき、追いかけてくる速度
+    float Dis;                      //カメラとターゲットの距離
     Vector3 Velocity;
 
     // Start is called before the first frame update
@@ -21,19 +21,26 @@ public class CameraControl : MonoBehaviour
     void Update()
     {
         //transform.LookAt(new Vector3(transform.localPosition.z, 0f, 0f));
-
-        //float Step = Speed * Time.deltaTime;
-
+        float Step = Speed * Time.deltaTime;
         Vector3 Pos = transform.position;
+
+        //Targetとの距離を求めて100倍する
+        Dis = Vector3.Distance(transform.position, Target.transform.position) * 100f;
+
+        //端数を切り捨てる
+        float Rounding = Mathf.Floor(Dis);
 
         Pos.x = Target.transform.position.x + V3.x;
         Pos.y = V3.y;
         Pos.z = Target.transform.position.z + V3.z;
 
+        //マウス左クリックしているときはカメラを動かさない
         if (!Input.GetMouseButton(0))
         {
-            //transform.position = Vector3.MoveTowards(transform.position, Pos, Step);
-            transform.position = Vector3.SmoothDamp(transform.position, Pos, ref Velocity, SmoothTime);
+            //ターゲットの近くにいる場合は、ターゲットと同じ動きをする（瞬間移動）
+            //ターゲットと離れているときは、特定の速度でターゲットのもとへ行く
+            if (Rounding / 100f > Dis) transform.position = Pos;
+            else transform.position = Vector3.MoveTowards(transform.position, Pos, Step);
         }
     }
 }
