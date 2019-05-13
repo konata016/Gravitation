f8fe638360abdbcbe075a07157f7258c01e52941
Unknown <konata016@gmail.com>
2019-05-14 02:57:37 +0900

    途中

---
+++ Gravitation/Assets/Script/Player/PlayerAnime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnime : MonoBehaviour
+{
+    Animator Anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) Anim.SetBool("Running", true);
+        else Anim.SetBool("Running", false);
+    }
+}
